0cc1ffb71518e863770e5fcac43ea9c9074d6ea1
Attention355 <823727097@qq.com>
2019-03-11 16:22:12 +0800

    尝试异步获取数据完成

--- app/controller/home.js
+++ app/controller/home.js
@@ -8,20 +8,29 @@ class HomeController extends Controller {
   async index() {
     const { ctx } = this;
     const url = 'http://bslc.lib.swjtu.edu.cn/uas/gethtml_zuowei.jsp';
-    http.get(url, function(res) {
-      const arrBuf = [];
-      let bufLength = 0;
-      res.on('data', function(chunk) {
-        arrBuf.push(chunk);
-        bufLength += chunk.length;
-      });
-      res.on('end', function() {
-        const chunkAll = Buffer.concat(arrBuf, bufLength);
-        var seatInfo = iconv.decode(chunkAll, 'gbk');
+    let seatInfo = '';
+
+    const httpget = new Promise(function(resolve, reject) {
+      http.get(url, function(res) {
+        const arrBuf = [];
+        let bufLength = 0;
+        res.on('data', function(chunk) {
+          arrBuf.push(chunk);
+          bufLength += chunk.length;
+        });
+        res.on('end', function() {
+          const chunkAll = Buffer.concat(arrBuf, bufLength);
+          seatInfo = iconv.decode(chunkAll, 'gbk');
+          resolve(seatInfo);
+        });
       });
     });
-    const data = await ctx.service.spider.infoProcess(seatInfo);
-    console.log(seatInfo)
+    httpget.then(async function() {
+      const res = await ctx.service.spider.infoProcess(seatInfo);
+      console.log(res);
+    });
+
+
   }
 }
 
